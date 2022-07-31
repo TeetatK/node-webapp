9b5e0396a0ad342240356b413d7bd68754393dd0
TeetatK <teetatk63@nu.ac.th>
2022-07-31 21:03:07 +0700

    First commit

--- utility.js
+++ utility.js
@@ -2,7 +2,7 @@
 
 const request = require("request-promise");
 const EXTERNAL_API="https://tools.ecpe.nu.ac.th/network/api/student/"; // put url
-const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjU4ODQ0NjcxLCJleHAiOjE2NTg4NDgyNzEsIm5iZiI6MTY1ODg0NDY3MSwianRpIjoiQkF1d3JJNTB6aXlWTmphZSIsInN1YiI6NywicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.x_BakIcOHAA0LEdDbuZRehUi0mSHaw26MfTFA3xdgi4"; // put access token
+const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjU5MjU5NjA2LCJleHAiOjE2NTkyNjMyMDYsIm5iZiI6MTY1OTI1OTYwNiwianRpIjoiTE15bnp4ZjBUbVNneXk2RSIsInN1YiI6NywicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.klyhhB7yEl2xK4c32QzSdbmanUE83ije1xjVWE7qbuo"; // put access token
 const student = {
   name: '', // replace with your full name.
   age: 19, // put your age.
