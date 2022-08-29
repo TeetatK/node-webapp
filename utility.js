74fcf8412d0f8546c42e7502586f01cc9d7a41f4
TeetatK <teetatk63@nu.ac.th>
2022-08-29 19:58:57 +0700

    update card

--- utility.js
+++ utility.js
@@ -2,11 +2,12 @@
 
 const request = require("request-promise");
 const EXTERNAL_API="https://tools.ecpe.nu.ac.th/network/api/student/"; // put url
-const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjYxNzAzMzc3LCJleHAiOjE2NjE3MDY5NzcsIm5iZiI6MTY2MTcwMzM3NywianRpIjoiQlV4amxNMWZkVktmNDEzeiIsInN1YiI6NywicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.MgRFA4wgG-1vsSFEPjtLD2qg7iUNaQzqJkRSSpJt06k"; // put access token
+const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjYxNzc1MzA1LCJleHAiOjE2NjE3Nzg5MDUsIm5iZiI6MTY2MTc3NTMwNSwianRpIjoiZUkxSnpORDU5VDdiRnFUSiIsInN1YiI6NywicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.-8moKdU1QdZPhg5qM61FdPy6Wv6NFT__sUk4899ners"; // put access token
 const student = {
-  name: '', // replace with your full name.
-  age: 19, // put your age.
-  gender: '', // replace with your gender
+  id_student: 6362860,
+  name: 'teetat', // replace with your full name.
+  age: 20, // put your age.
+  gender: 'male', // replace with your gender
   department: 'CPE'
 };
 
@@ -34,9 +35,4 @@ exports.findStudentbyId = function (student_id, cb) {
                 "Error": err.message
             });
         });
-}
-
-exports.fakeStudentbyInfo = function (student_id, cb) {
-
-  cb(student);
 }
